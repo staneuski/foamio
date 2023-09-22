d1cec4b924a3d84264ad44afa571924545d028bc
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2023-09-22 09:56:48 +0300

    `addons…functionObjects`: add custom `#includeEtc` dictionaries
    To make those functions searchable set `$WM_PROJECT_SITE` OpenFOAM environment variable to addons, e.g.:
    `export WM_PROJECT_SITE=$HOME/Documents/foamio/addons`

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ URL = 'https://github.com/StasF1/foamio'
 EMAIL = 'stanislau.stasheuski@gmail.com'
 AUTHOR = 'Stanislau Stasheuski'
 REQUIRES_PYTHON = '>=3.6'
-VERSION = '0.4.1'
+VERSION = '0.4.2'
 
 REQUIRED = [
     'matplotlib',
